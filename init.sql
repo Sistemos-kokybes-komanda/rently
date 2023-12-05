bfed3bf9b66a70cf78e570de1be259d497ba7dff
Tomas <tomas.solovej@gmail.com>
2023-12-05 13:27:05 +0200

    populate db script

--- init.sql
+++ init.sql
@@ -50,3 +50,26 @@ ALTER TABLE
     `reservations` ADD CONSTRAINT `reservations_garage_id_foreign` FOREIGN KEY(`garage_id`) REFERENCES `garages`(`id`);
 ALTER TABLE
     `reviews` ADD CONSTRAINT `reviews_reservation_id_foreign` FOREIGN KEY(`reservation_id`) REFERENCES `reservations`(`id`);
+-- Populate users table
+INSERT INTO `users` (`name`, `surname`, `email`, `password`, `phone_number`, `description`)
+VALUES
+    ('John', 'Doe', 'john.doe@example.com', 'password123', '+123456789', 'A user with a description'),
+    ('Alice', 'Smith', 'alice.smith@example.com', 'pass456word', '+987654321', NULL);
+
+-- Populate garages table
+INSERT INTO `garages` (`summary`, `address`, `price`, `owner_id`)
+VALUES
+    ('Garage 1', '123 Main St', 50.00, 1),
+    ('Garage 2', '456 Oak St', 40.00, 2);
+
+-- Populate reservations table
+INSERT INTO `reservations` (`user_id`, `garage_id`, `start_date`, `end_date`)
+VALUES
+    (1, 2, '2023-01-01 08:00:00', '2023-01-02 18:00:00'),
+    (2, 1, '2023-02-15 12:00:00', '2023-02-17 15:00:00');
+
+-- Populate reviews table
+INSERT INTO `reviews` (`reservation_id`, `rating`, `comment`)
+VALUES
+    (1, 4, 'Great experience!'),
+    (2, 5, 'Excellent service and clean garage');
